c2e4f9a7407f570a1ba30f925d64fa6d714f81a8
shenjia <zshenjia@gmail.com>
2019-04-29 03:18:53 +0800

    Update git_pull.sh

--- deploy/git_pull.sh
+++ deploy/git_pull.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-cd /www/saolei.net/deploy/saolei.net-2008
-git pull origin test
+REPO=/www/saolei.net/deploy/saolei.net-2008
+LOG=/www/saolei.net/deploy/git.log
+
+echo "-------------------------------------" >> $LOG
+date >> $LOG
+cd $REPO
+OLD_COMMIT=`git log|head -n 1|sed 's/commit //'`
+git pull origin test >> $LOG 2>&1
+NEW_COMMIT=`git log|head -n 1|sed 's/commit //'`
+
+if [ "$OLD_COMMIT" = "$NEW_COMMIT" ];then
+    echo "no new commit!"
+else
+    echo "$OLD_COMMIT -> $NEW_COMMIT"
+fi
